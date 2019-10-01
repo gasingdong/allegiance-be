bdbfb229791095497c51b40126aba52518a80605
Ang Xu <angxu1234@gmail.com>
2019-09-30 19:28:41 -0500

    update backend authConfig

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -3,23 +3,23 @@ const jwksRsa = require("jwks-rsa");
 
 // Set up Auth0 configuration
 const authConfig = {
-  domain: "dev-uzdmt05n.auth0.com",
-  audience: "https://dev-uzdmt05n.auth0.com/api/v2/"
+	domain: "dev-5xt3fb-m.auth0.com",
+	audience: "https://dev-5xt3fb-m.auth0.com/api/v2/"
 };
 
 // Define middleware that validates incoming bearer tokens
 // using JWKS from YOUR_DOMAIN
 const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
-  }),
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+	}),
 
-  audience: authConfig.audience,
-  issuer: `https://${authConfig.domain}/`,
-  algorithm: ["RS256"]
+	audience: authConfig.audience,
+	issuer: `https://${authConfig.domain}/`,
+	algorithm: ["RS256"]
 });
 
 module.exports = checkJwt;
