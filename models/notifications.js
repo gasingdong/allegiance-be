fb26702a396395859340ddc5c884b2e1a97c310c
Brandon Dong <gasingdong@gmail.com>
2019-10-16 15:21:57 -0700

    notification model changes to fix errors

--- models/notifications.js
+++ models/notifications.js
@@ -1,14 +1,40 @@
 const db = require("../data/db-config");
 
+const Posts = require("../models/posts");
+
 module.exports = {
   findByUserId,
   addToUser
 };
 
-function findByUserId(user_id) {
-  return db("notifications")
-    .join("users", "users.id", "invoker_id")
-    .where({ user_id });
+async function findByUserId(user_id) {
+  const results = await db("notifications as n")
+    .join("users as u", "u.id", "n.invoker_id")
+    .where({ user_id })
+    .select(
+      "n.user_id",
+      "n.invoker_id",
+      "n.type_id",
+      "n.type",
+      "n.read",
+      "n.created_at",
+      "u.username",
+      "u.first_name",
+      "u.last_name",
+      "u.image",
+      "n.id");
+  
+  return Promise.all(results.map(async (note) => {
+    try {
+      const post = await Posts.find({ "p.id": note.type_id }).first();
+      return {
+        ...note,
+        post_content: post.post_content
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }));
 }
 
 function addToUser(user_id, invoker_id, type_id, type) {
