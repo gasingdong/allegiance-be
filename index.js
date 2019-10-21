61ce79b25060ca5bd4ed791f0b32faa5cf64ef38
Connor Claxton <czclaxton@gmail.com>
2019-10-21 18:09:56 -0500

    finished socketio integration

--- index.js
+++ index.js
@@ -7,11 +7,27 @@ const PORT = process.env.PORT || 5000;
 const http = require("http").createServer(server);
 const io = require("socket.io")(http);
 
+const clients = {};
+
 io.on("connection", socket => {
-  console.log("New connection");
+  console.log("New connection id: " + socket.id);
 
   socket.on("join", data => {
-    console.log("something", data);
+    console.log("received", data);
+    clients[data.id] = socket.id;
+    console.log(clients);
+  });
+
+  socket.on("send notification", data => {
+    console.log("send notification");
+    data.userIds.forEach(id => {
+      const socketid = clients[id];
+      io.to(socketid).emit("new notification");
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected id: " + socket.id);
   });
 });
 
--- models/notifications.js
+++ models/notifications.js
@@ -1,6 +1,7 @@
 const db = require("../data/db-config");
 
 const Posts = require("../models/posts");
+const Replies = require("../models/replies");
 
 module.exports = {
   findByUserId,
@@ -24,19 +25,30 @@ async function findByUserId(user_id) {
       "u.first_name",
       "u.last_name",
       "u.image",
-      "n.id");
-  
-  return Promise.all(results.map(async (note) => {
-    try {
-      const post = await Posts.find({ "p.id": note.type_id }).first();
-      return {
-        ...note,
-        post_content: post.post_content
+      "n.id"
+    );
+
+  return Promise.all(
+    results.map(async note => {
+      try {
+        if (note.type === "reply_like") {
+          const reply = await Replies.find({ "r.id": note.type_id }).first();
+          return {
+            ...note,
+            reply_content: reply.reply_content
+          };
+        } else {
+          const post = await Posts.find({ "p.id": note.type_id }).first();
+          return {
+            ...note,
+            post_content: post.post_content
+          };
+        }
+      } catch (err) {
+        console.log(err);
       }
-    } catch (err) {
-      console.log(err);
-    }
-  }));
+    })
+  );
 }
 
 function addToUser(user_id, invoker_id, type_id, type) {
@@ -49,9 +61,13 @@ function addToUser(user_id, invoker_id, type_id, type) {
 }
 
 function find(id) {
-  return db('notifications').where({ id }).first();
+  return db("notifications")
+    .where({ id })
+    .first();
 }
 
 function remove(id) {
-  return db('notifications').where({ id }).del();
+  return db("notifications")
+    .where({ id })
+    .del();
 }
