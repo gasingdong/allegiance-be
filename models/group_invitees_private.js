c0b3c36d548e0248fe9222d819c1aa1f5c9d2a6a
Sean Doyle <sdoylelambda@gmail.com>
2019-10-25 16:34:31 -0400

    Model for private group invite.

---
+++ models/group_invitees_private.js
@@ -0,0 +1,32 @@
+const db = require('../data/db-config');
+
+module.exports = {
+    findByUserId,
+    findGroupById,
+    privateInvitation,
+    deleteInvitation,
+}
+
+function findByUserId(user_id){
+    return db('join_private_group').where({ user_id });
+}
+
+function findGroupById(group_id){
+    return db('join_private_group as g')
+        .where({ group_id })
+        .join('users as u', 'u.id', 'g.user_id')
+        .select('u.email', 'u.username');
+}
+
+function privateInvitation(user_id, group_id, sender_id) {
+    return db('join_private_group')
+        .insert({ group_id, user_id, sender_id })
+        .returning('*');
+}
+
+function deleteInvitation(group_id, user_id, sender_id) {
+    return db("group_invitees")
+      .where({ group_id, user_id, sender_id })
+      .del()
+      .returning("*");
+  }
