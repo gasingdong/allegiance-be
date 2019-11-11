3313524802f5a5265f2fb3284c08265a3a615511
luis <mrluismartinezzz@gmail.com>
2019-11-10 20:53:07 -0800

    [update] replaced http server with the return value of express server.listen

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 require("express-async-errors");
-const server = require("./api/server");
 
 const PORT = process.env.PORT || 5000;
 
-const http = require("http").createServer(server);
-const io = require("socket.io")(http);
+const server = require("./api/server").listen(PORT, () => {
+  console.log(`\n* Server Running on port ${PORT} *\n`);
+});
+
+const io = require("socket.io")(server);
 
 const clients = {};
 
@@ -39,8 +41,4 @@ io.on("connection", socket => {
   socket.on("disconnect", () => {
     console.log("disconnected id: " + socket.id);
   });
-});
-
-http.listen(PORT, () => {
-  console.log(`\n* Server Running on port ${PORT} *\n`);
 });
