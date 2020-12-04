3f7533f80fd1e61685609418d27adf458590f54a
gasingdong <gasingdong@gmail.com>
2020-12-04 05:34:29 -0800

    tweaks to workflow

--- pgclient.js
+++ pgclient.js
@@ -1,6 +1,8 @@
 const { Client } = require('pg');
 
 const pgclient = new Client({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   user: 'postgres',
   password: 'postgres',
 });
